f7ff0d5f077cfc04369de1054d907c7ef169ad68
Decko <deckocol@gmail.com>
2023-06-17 02:02:36 -0500

    new

---
+++ script.js
@@ -0,0 +1,73 @@
+//Audio
+document.addEventListener('DOMContentLoaded', function () {
+  var audio = document.getElementById('miAudio');
+  var isPlaying = false;
+
+  document.body.addEventListener('mouseover', function () {
+    if (!isPlaying) {
+      audio.volume = 0.05;
+      try {
+        audio.play();
+        isPlaying = true;
+      } catch (error) {
+        console.log("La reproducción automática del audio fue bloqueada por el navegador.");
+      }
+    }
+  });
+
+  document.body.addEventListener('click', function () {
+    if (isPlaying) {
+      audio.muted = true;
+      isPlaying = false;
+    } else {
+      audio.volume = 0.05;
+      audio.muted = false;
+      isPlaying = true;
+    }
+  });
+});
+
+
+
+
+//CURSOR LIGHT
+
+window.onpointermove = event => {
+  const { clientX, clientY } = event;
+
+  glow.style.left = `${clientX}px`; /* Utiliza 'style.left' en lugar de 'animate' */
+  glow.style.top = `${clientY}px`; /* Utiliza 'style.top' en lugar de 'animate' */
+};
+
+//LIGHT EFFECTS
+
+function changeGlowColor() {
+  var glow = document.getElementById("glow");
+  glow.classList.add("hover");
+}
+
+function resetGlowColor() {
+  var glow = document.getElementById("glow");
+  glow.classList.remove("hover");
+}
+
+
+
+
+
+
+
+
+  //fullscreen
+
+  function toggleFullscreen() {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+      document.documentElement.msRequestFullscreen();
+    }
+  }
