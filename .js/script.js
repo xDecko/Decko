098ccea57e9a4fb587edc7600e3abea01e03f369
Decko <deckocol@gmail.com>
2023-06-20 19:41:37 -0500

    Pre Icons & bubbles

--- .js/script.js
+++ .js/script.js
@@ -74,3 +74,20 @@ function resetGlowSize() {
       document.documentElement.msRequestFullscreen();
     }
   }
+
+
+  //Text animation
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
